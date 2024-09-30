07891305d52d0b770200fef4b2daf9fc1ac20f06
JiinYang01 <javaeclipse1203@naver.com>
2024-09-30 23:22:08 +0900

    [Gold V] Title: 평범한 배낭, Time: 232 ms, Memory: 189588 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,16 +1,18 @@
 a,b=map(int,input().split())
-grp=[[0,0]]
+grp=[[]]
+for i in range(a):
+    x=list(map(int,input().split()))
+    grp.append(x)
+grp.sort()
 dp=[[0]*(b+1) for _ in range(a+1)]
-for _ in range(1,a+1):
-    x,y=map(int,input().split())
-    grp.append([x,y])
 
 for i in range(1,a+1):
     w=grp[i][0]
     v=grp[i][1]
     for j in range(1,b+1):
-        if j<w:
+        if w>j:
             dp[i][j]=dp[i-1][j]
         else:
-            dp[i][j]=max(dp[i-1][j],dp[i-1][j-w]+v)
+            dp[i][j]=max(v+dp[i-1][j-w],dp[i-1][j])
+
 print(dp[a][b])
