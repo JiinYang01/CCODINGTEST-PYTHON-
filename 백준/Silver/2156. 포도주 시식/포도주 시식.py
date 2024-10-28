8e123757e4956d05935d0d386a62c9c44033099f
JiinYang01 <javaeclipse1203@naver.com>
2024-10-29 02:26:24 +0900

    [Silver I] Title: 포도주 시식, Time: 120 ms, Memory: 111724 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -15,6 +15,6 @@ if a>3:
     dp[3]=max(grp[0]+grp[2]+grp[3],grp[0]+grp[1]+grp[3],grp[1]+grp[2])
 if a>4:
     for i in range(4,a):
-            dp[i]=max(dp[i-4]+grp[i-2]+grp[i-1],dp[i-3]+grp[i-1]+grp[i],dp[i-2]+grp[i],dp[i-3]+grp[i-1])
+            dp[i]=max(dp[i-4]+grp[i-2]+grp[i-1],dp[i-3]+grp[i-1]+grp[i],dp[i-2]+grp[i])
 
 print(dp[a-1])
