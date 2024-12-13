6b7c926dcd5fe9fc6e7f63aea7a4e4f2c972c017
JiinYang01 <javaeclipse1203@naver.com>
2024-12-13 22:35:07 +0900

    [Silver IV] Title: 수들의 합 2, Time: 100 ms, Memory: 110788 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
@@ -1,12 +1,20 @@
-from itertools import combinations
 a,b=map(int,input().split())
-x=list(map(int,input().split()))
-j=[0]*(a+1)
-for i in range(a):
-    j[i+1]=j[i]+x[i]
-a=combinations(j,2)
-cnt=0
-for i in a:
-    if (i[1]-i[0])==b:
-       cnt+=1
-print(cnt) 
+grp=list(map(int,input().split()))
+s=0
+e=0
+an=0
+ans=0
+while s<a and s<=e:
+    while an<b and e<a:
+        an+=grp[e]
+        e+=1
+    if an==b:
+        ans+=1
+    if an>=b:
+        an-=grp[s]
+        s+=1
+    if e>=a and an<b:
+        break
+
+
+print(ans)
